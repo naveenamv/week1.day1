27c8e1e7203487ce8022b2b666287954c5e9684c
naveenamv <107459956+naveenamv@users.noreply.github.com>
2022-06-14 13:15:07 +0530

    Add files via upload

---
+++ FibbonacciSeries.java
@@ -0,0 +1,22 @@
+package Assignments.week1;
+
+public class FibbonacciSeries {
+	
+	public static void main(String[] args) {
+		int range=8;
+		int firstNum=0;
+		int secNum=1;
+		int sum;
+		System.out.println("FirstNumber : "+firstNum);
+		for(int i=1;i<=range;i++) {
+			sum=firstNum+secNum; 
+			firstNum=secNum; 
+			secNum=sum; 
+			
+			System.out.println("Sum"+sum);
+			
+		}
+		
+	}
+
+}
